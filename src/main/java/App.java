ccec4f9460df7d33ca4cfda0e7266b708a8fd83b
JavaWebsite <alrubayeabbas@gmail.com>
2022-08-05 06:49:33 -0400

    First Created

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,5 +2,7 @@
 public class App {
     public static void main(String[] args) {
 
+        ErrorMessage.SHOW("its an error");
+
     }
 }
---
+++ src/main/java/ErrorMessage.java
@@ -0,0 +1,6 @@
+public class ErrorMessage {
+
+    public static void SHOW(String err){
+        System.out.println(err);
+    }
+}
